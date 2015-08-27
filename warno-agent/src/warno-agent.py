c0b18bde07d00c892c7486c6f0bccdb733209c4b
schu970 <eddie.schuman@pnnl.gov>
2015-08-27 10:17:57 -0700

    Integrated the merged user portal into warno-vagrant services.
    Now correctly runs with the nginx proxy service and the database.
    
    Pretty much fully contained within vagrant now.
    
    To access functionality locally, had to add two lines to /etc/hosts
    192.168.50.100  warno-user-portal.local
    192.168.50.100  warno-event-manager.local
    Nginx sorts out which server to send user to depending on the hostname.
    
    All 3 servers (nginx, user-portal, event-manager) running on port 80.
    
    Updated .gitignore to ignore the Miniconda, anaconda, and database_dump files again.
    
    Had to add some placeholder files for radar-agent to prevent the docker-compose.yml
    build from breaking. (warno-agent/  requirements.txt and warno-agent.py)

---
+++ warno-agent/src/warno-agent.py
@@ -0,0 +1,1 @@
+print "\n\n\n\n\n Warno Agent Placeholder Ran \n\n\n\n\n\n"
--- warno-user-portal/src/RadarStatusMonitor.py
+++ warno-user-portal/src/RadarStatusMonitor.py
@@ -625,8 +625,9 @@ def new_log():
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    # Temporary home page
+    return redirect(url_for('show_radar_status'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5555)
+    app.run(debug=True, host='0.0.0.0', port=80)
