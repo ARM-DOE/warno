a66b08d4c7037fbe4bba0b6d632c45cf71ed9c01
Joseph C. Hardin <joseph.hardin@pnnl.gov>
2015-10-01 16:03:01 -0700

    no idea....again

---
+++ warno-agent/src/plugins/test.py
@@ -0,0 +1,7 @@
+
+def register():
+    return 1
+
+def run(msg_queue):
+    msg_queue.put("test")
+
---
+++ warno-agent/src/plugins/test3.py
@@ -0,0 +1,7 @@
+plugins/test2.py:4:1: E302 expected 2 blank lines, found 0
+plugins/test2.py:9:23: E231 missing whitespace after ':'
+plugins/test2.py:13:1: E302 expected 2 blank lines, found 1
+plugins/test2.py:19:5: E303 too many blank lines (2)
+plugins/test2.py:19:21: E231 missing whitespace after ','
+plugins/test2.py:20:9: E113 unexpected indentation
+plugins/test2.py:31:1: W391 blank line at end of file
