941b79eaf618c6bfc054d06f8a97969037e7c0cc
Eddie Schuman <eddie.schuman@pnnl.gov>
2015-10-08 16:25:04 -0700

    Not Working. Event Manager mostly clean/functional, agent is not.
    Will not run right out of the repository.  Event managers/agent are set
    to run localhost rather than within vagrant right now.  My working
    environment was 2 copies of the event manager folder, one with a config.yml
    that said it was the central facility and one that did not, with the central
    facility manager being hosted on localhost port 5001 instead of 5000.
    
    Working
    +Agent request event code / propagates up chain / created if not exists
    passed back down chain and saved
    +Site id request goes up and down the chain, with the Site agent saving
    all of the information if it did not already have it
    +Same thing for instruments
    +If event code passed to managers is not a "special" code (meaning its
    own processing chain) the event is saved in both the Site and CF Managers
    +One agent plugin pulls the status of ena-kazr and stores the attributes as
    individual events(proof of concept/functionality, will eventually be one
    large event saved in its specialized table).
    
    Those are the currently recalled highlights.  Next item to do is to have a
    special event code for the PAFClient saves in the database (and saving func)

--- warno-agent/src/plugins/test2.py
+++ warno-agent/src/plugins/test2.py
@@ -1,29 +1,34 @@
 import time
-import pyarmret
-from pyarmret.io.PAFClient import PAFClient
-def register():
-    event_id = 1
-    event_table = None
-    event_name = "test_event2"
 
-    return {"event_id" : event_id,
-            "event_table": event_table,
-            "event_name": event_name}
+def register(msg_queue):
+    print "Registering Secondary"
+    event_code_names = ["temperature", "zenith", "voltage", "sidelobe", "central"]
+    instrument_name = "SACR-2"
 
+    # for e_type in event_types:
+    #     print "Requesting Event code"
+    #     msg_queue.put('{"Event_Code": 1, "Data": "%s"}' % e_type)
+    print "Done Registering Secondary"
+    return {"instrument_name": instrument_name, "event_code_names": event_code_names}
+
+i = 0
 def run(msg_queue):
-    pafc = PAFClient('ena-kazr', 3000)
-    pafc.connect()
-    pafc.get_server_info()
-
-    for i in range(0,10)
-        try:
-            msg = pafc.get_text_configuration()
-            msg_queue.put(msg)
-        except Exception, e:
-            msg_queue.put(e)
-        else:
-            pass
-        finally:
-            pass
-
-        time.sleep(2)
+    print "Running Secondary"
+    i = 0
+    while True:
+        i + 1
+        timestamp = time.mktime(time.localtime())
+        msg_queue.put('{"event": "temperature", "data": {"Instrument_Id": 1, "Time": %s, "Value": "%s"}}' % (timestamp, i))
+        msg_queue.put('{"event": "zenith", "data": {"Instrument_Id": 1, "Time": %s, "Value": "%s"}}' % (timestamp, i))
+        msg_queue.put('{"event": "voltage", "data": {"Instrument_Id": 1, "Time": %s, "Value": "%s"}}' % (timestamp, i))
+        msg_queue.put('{"event": "sidelobe", "data": {"Instrument_Id": 1, "Time": %s, "Value": "%s"}}' % (timestamp, i))
+        msg_queue.put('{"event": "central", "data": {"Instrument_Id": 1, "Time": %s, "Value": "%s"}}' % (timestamp, i))
+        time.sleep(5)
+        # msg_queue.put('{"Tick": %i}' % i)
+        # time.sleep(6)
+        # pafc = PAFClient('ena-kazr', 3000)
+        # pafc.connect()
+        # timestamp = time.mktime(time.localtime())
+        # if i < 4:
+        #     print("Sending Value")
+        #     msg_queue.put('{"Event_Code": 4, "Data": {"Instrument_Id": 1, "Time": %s, "Value": %s}}' % (timestamp, i))
