cf198e8d7d9ab516530ed410c2f849a1a796c814
Eddie Schuman <eddie.schuman@pnnl.gov>
2016-01-15 15:37:31 -0800

    Renamed logs.data and log.schema to instrument_logs*, unification helps with the utility functions and general readability.

--- warno-event-manager/src/database/populate_demo_db.py
+++ warno-event-manager/src/database/populate_demo_db.py
@@ -5,17 +5,17 @@ cur = db.cursor()
 
 # For each entry, first entry is table name, second entry is demo data for the table
 table_data = [
-                   ["instruments", "instruments.data"],
-                   ["instrument_logs", "logs.data"],
-                   ["prosensing_paf", "prosensing_paf.data"],
-                   ["event_codes", "event_codes.data"],
-                   ["events_with_text", "events_with_text.data"],
-                   ["events_with_value", "events_with_value.data"],
-                   ["pulse_captures", "pulse_captures.data"],
-                   ["table_references", "table_references.data"],
-                   ["instrument_data_references", "instrument_data_references.data"]
+                   "instruments",
+                   "instrument_logs",
+                   "prosensing_paf",
+                   "event_codes",
+                   "events_with_text",
+                   "events_with_value",
+                   "pulse_captures",
+                   "table_references",
+                   "instrument_data_references"
                ]
 
 for table in table_data:
-    if utility.table_exists(table[0], cur):
-        utility.load_data_into_table("schema/%s" % table[1], table[0], db)
+    if utility.table_exists(table, cur):
+        utility.load_data_into_table("schema/%s.data" % table, table, db)
