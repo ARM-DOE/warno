e8949db50a7f864f7fb36a990e040d31f53b2485
Joseph C. Hardin <joseph.hardin@pnnl.gov>
2016-01-22 16:12:04 -0800

    Created directory for selenium tests.

--- selenium_tests/test_basic.py
+++ selenium_tests/test_basic.py
@@ -2,15 +2,17 @@ from unittest import TestCase
 from selenium import webdriver
 
 
-class TestBasicFunctionality(TestCase):
-    self.warno_url = 'yggdrasil.pnl.gov'
+class TestIndexFunctionality(TestCase):
+    warno_url = 'http://yggdrasil.pnl.gov:8080'
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        # self.browser.implicitly_wait(1)
 
     def tearDown(self):
         self.browser.quit()
 
+    def test_index_has_WARNOin_title(self):
+        self.browser.get(self.warno_url)
+        self.assertTrue('WARNO' in self.browser.title, 'WARNO is not in index title.')
 
-    def test_index_has_title(self):
-        self.browser.get(warno_url)
