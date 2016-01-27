fed5b52e0b508fa5b8b73cc1b0079fa7f7d3c4c7
Eddie Schuman <eddie.schuman@pnnl.gov>
2016-01-26 17:43:26 -0800

    Added a couple of working mocked flask unit tests.

--- warno_user_portal/src/tests/test_views.py
+++ warno_user_portal/src/tests/test_views.py
@@ -1,7 +1,39 @@
+import flask
+import mock
+import requests
 from unittest import TestCase
 from UserPortal import views
 
 class test_views(TestCase):
+
+  @mock.patch('psycopg2.connect')
+  def test_get_instruments_1(self, connect):
+    with views.app.test_client() as c:
+      connection = connect.return_value
+      cursor = connection.cursor.return_value
+      result = c.get("/instruments/1")
+      execute_calls = cursor.execute.call_args_list
+      # cursor.execute.assert_called_with("SELECT i.instrument_id, i.name_short,"
+      #           + " i.name_long, i.type, i.vendor, i.description, i.frequency_band,"
+      #           + " s.name_short, s.latitude, s.longitude, s.site_id FROM instruments"
+      #           + " i JOIN sites s ON (i.site_id = s.site_id) WHERE i.instrument_id = 1")
+      self.assertTrue(mock.call("SELECT special, description FROM instrument_data_references WHERE instrument_id = %s",(u'1',)) in execute_calls)
+      self.assertEqual(result.status, '200 OK')
+      # self.assertEqual(True, False)
+
+  @mock.patch('psycopg2.connect')
+  def test_delete_instruments_1(self, connect):
+    with views.app.test_client() as c:
+      connection = connect.return_value
+      cursor = connection.cursor.return_value
+      result = c.delete("/instruments/1")
+      execute_calls = cursor.execute.call_args_list
+      self.assertTrue(mock.call("DELETE FROM instruments WHERE instrument_id = %s",(u'1',)) in execute_calls)
+      self.assertEqual(result.status, '200 OK')
+      # self.assertEqual(True, False)
+
+
+
 
   def test_is_number(self):
     self.assertTrue(views.is_number('1'), '"1 should be a number"')
