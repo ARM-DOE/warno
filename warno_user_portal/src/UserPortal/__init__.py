363dc39e048a370da492034cb12a9bda1ee59f4d
Joseph C. Hardin <joseph.hardin@pnnl.gov>
2016-02-08 12:32:52 -0800

    Split instrument subanchors out into it's own blueprint.

--- warno_user_portal/src/UserPortal/__init__.py
+++ warno_user_portal/src/UserPortal/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 from UserPortal.users import users
 from UserPortal.sites import sites
+from UserPortal.instruments import instruments
 
 app = Flask(__name__)
 import UserPortal.views
 
 app.register_blueprint(users)
 app.register_blueprint(sites)
+app.register_blueprint(instruments)
