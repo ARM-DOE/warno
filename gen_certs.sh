116690db795acbea0e5b784afc29d234fe2e4237
Eddie Schuman <eddie.schuman@pnnl.gov>
2016-03-02 14:13:57 -0800

    Split self signed cert to use custom Certificate Authority. Allows browser trust.

--- gen_certs.sh
+++ gen_certs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-openssl genrsa -out proxy/privkey.pem 2048
-openssl req -new -x509 -key proxy/privkey.pem -out proxy/cacert.pem -days 1095
+openssl genrsa -out privkey.pem 2048
+openssl req -new -key proxy/privkey.pem -out privreq.csr
+openssl x509 -req -in privreq.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out proxy/cacert.pem -days 1095 -sha256
