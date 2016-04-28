dc8c6fcdace03c47562a3bec11549675e4f275e8
Joseph C. Hardin <josephhardinee@gmail.com>
2016-04-28 10:04:41 -0700

    Fixed bug in utility_setup_scripts to use the correct box label from yggdrasil

--- utility_setup_scripts/set_up_images.sh
+++ utility_setup_scripts/set_up_images.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-wget http://yggdrasil.pnl.gov/warno1.box -N
+wget http://yggdrasil.pnl.gov/warno2.box -N
 vagrant box add warnobox1 warno2.box --force
