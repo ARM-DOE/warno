823f76471824be31a3269def1cea9a1e68fce00b
Eddie Schuman <eddie.schuman@pnnl.gov>
2016-04-28 08:02:26 -0700

    Minor fixes to dockerless startup/deploy.

--- utility_setup_scripts/start_all_servers.sh
+++ utility_setup_scripts/start_all_servers.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Otherwise config cannot be found (unset PYTHONPATH, DATA_STORE_PATH)
+# Otherwise config cannot be found (not set PYTHONPATH, DATA_STORE_PATH)
 echo "Start All Servers"
-source $DIR/../set_env_for_testing.sh
+source /vagrant/utility_setup_scripts/set_env_for_testing.sh
 
 # Postgres server is started in another script, as it requires sudo access
-bash $DIR/../warno_event_manager/start_event_manager.sh
-bash $DIR/../warno_user_portal/start_user_portal.sh
-bash $DIR/../warno_agent/start_agent.sh
+bash /vagrant/warno_event_manager/start_event_manager.sh
+bash /vagrant/warno_user_portal/start_user_portal.sh
+bash /vagrant/warno_agent/start_agent.sh
